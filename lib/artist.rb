44af2f20978e98b0dd62d3d4d287b7c1581526c7
Travis T <travistokuyama@gmail.com>
2018-09-26 20:32:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,7 +19,7 @@ class Artist
     Song.all.select {|song| song.artist == self }
   end
   
-  def genres 
-    Genre.all.select {|genre| genre.artist == self}
+  def genres
+    songs.map(&:genre)
   end
 end
