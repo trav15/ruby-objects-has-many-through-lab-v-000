e7f050c5c2f118c2d056babc482b4de9cb405640
Travis T <travistokuyama@gmail.com>
2018-09-26 20:20:17 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,16 @@
-class Genre 
+class Genre  
+  attr_accessor :genre, :song, :artist
+  @@all = []
   
+  def initialize(genre, song, artist)
+    @genre = genre 
+    @song = song
+    @artist = artist 
+
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
