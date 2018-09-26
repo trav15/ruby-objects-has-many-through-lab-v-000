4bab87fcca04acc9358da17fca80402ddb289eea
Travis T <travistokuyama@gmail.com>
2018-09-26 20:16:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,5 +3,13 @@ class Song
   @@all = []
   
   def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
